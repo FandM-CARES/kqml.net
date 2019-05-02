b99b5972f881a1e369b2fe99644a38af38f4308d
Huhuhu812 <099fe119@opayq.com>
2019-05-02 15:07:57 -0500

    useless KQMLModule skeleton

--- KQML/KQMLModule.cs
+++ KQML/KQMLModule.cs
@@ -1,10 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace KQML
 {
     class KQMLModule
     {
+        public Dictionary<object, object> Defaults;
+        public KQMLDispatcher Dispatcher;
+        public int MAX_PORT_TRIES;
+        public int ReplyIdCounter;
+
+        public KQMLModule()
+        {
+
+        }
+
+        public void ReceiveEof()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void HandleException(IOException e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ReceiveMessageMissingVerb(KQMLObject msg)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public void ReceiveMessageMissingContent(KQMLObject msg)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void ReceiveOtherPerformative(KQMLObject msg)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
