685e98d28463df1ead2c87f8a035e589fe518e17
Willie Wilson <jrw@northwestern.edu>
2019-05-07 19:24:12 -0500

    beginings of some socket stuff

--- KQML/KQMLModule.cs
+++ KQML/KQMLModule.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Net.Sockets;
 using System.Text;
 
 namespace KQML
@@ -21,13 +22,16 @@ namespace KQML
         public string Name;
         public bool ScanForPort;
         public bool Debug;
+        public StreamReader Out;
+        public KQMLReader Inp;
+        public string GroupName;
 
         public KQMLModule()
         {
             Host = "localhost";
             Port = 6200;
             IsApplication = false;
-            Testing = False;
+            Testing = false;
             Socket = null;
             Debug = false;
 
@@ -35,12 +39,123 @@ namespace KQML
             MAX_PORT_TRIES = 100;
             ReplyIdCounter = 1;
 
-            //TODO: Neds to handle command line argument and change defaults
+            //TODO: Needs to handle command line argument and change defaults
         
-            if(!Testing) {
+            if(!Testing)
+            {
+                Out = null;
+                Inp = null;
+                //TODO: Log here
+                bool conn = Connect(Host, Port);
+                if(!conn)
+                {
+                    //TODO: Log error
+                    Exit(-1);
+                }
+                //UNDONE: use if to check for assert invariant
+               
+               
 
+            }
+            else
+            {
+                //TODO: Using stdio connection
+                //UNDONE: what is this bytes stuff doing
+                //Maybe not put test code here at all 
+            }
+            Dispatcher = new KQMLDispatcher(this, Inp, Name);
+            Register();
+        }
 
-            }                        
+        private void Exit(int v)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Start()
+        {
+            if (!Testing)
+                Dispatcher.Start();
+        }
+
+        public void SubscribeRequest(string reqType)
+        {
+            KQMLPerformative msg= new KQMLPerformative("subscribe");
+            KQMLList content = new KQMLList("request)");
+            content.Append("&key");
+            content.Set("content", KQMLList.FromString($"({reqType} . *)"));
+            msg.Set("content", content);
+            Send(msg);
+        }
+
+        public void SubscribeTell(string tellType)
+        {
+            KQMLPerformative msg = new KQMLPerformative("subscribe");
+            KQMLList content = new KQMLList("tell");
+            content.Append("&key");
+            content.Set("content", KQMLList.FromString($"({tellType} . *)"));
+            msg.Set("content", content);
+            Send(msg);
+        }
+        public bool Connect(string host = null, int startPort = 0)
+        {
+            if (string.IsNullOrEmpty(host))
+                host = Host;
+            if (startPort == 0)
+                startPort = Port;
+            if (!ScanForPort)
+                return Connect1(host, startPort, true);
+            else
+            {
+                int maxtries = MAX_PORT_TRIES;
+                for (int port = startPort; port < startPort + MAX_PORT_TRIES; port++)
+                {
+                    bool conn = Connect1(host, port, false);
+                    if (conn)
+                        return true;
+
+                }
+                //TODO: Log error: failed to connect
+                return false;
+            }
+        }
+
+        private bool Connect1(string host, int port, bool verbose)
+        {
+            try
+            {
+                //Crreate Socket
+                Socket.Connect(host, port);
+                // TODO: Socket magic!
+                return true;
+
+            }
+            catch (SocketException e)
+            {
+                if(verbose)
+                {
+                    //Log error with e
+                }
+                return false;
+            }
+        }
+
+        private void Send(KQMLPerformative msg)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Register()
+        {
+            if(!string.IsNullOrEmpty(Name))
+            {
+                KQMLPerformative perf = new KQMLPerformative("register");
+                perf.Set("name", Name);
+                if (string.IsNullOrEmpty(GroupName))
+                {
+
+                }
+            }
         }
 
         public void ReceiveEof()
